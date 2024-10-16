caf80f7a3e8659f954af341b75ac8da9991d3f9f
lau7dev <103751178+lau7dev@users.noreply.github.com>
2024-10-16 11:36:27 +0000

    up

---
+++ 2 - Files/letturafile.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    char c;
+    char NomeFile[] = {"Files/ciao.txt"};
+    int err;
+
+    FILE *pfile;
+    err = fopen_s(&pfile, NomeFile, "r");
+
+    if(err == 0){
+        while(!feof(pfile)){
+            c = fgetc(pfile);
+            printf("%c", c);
+        }
+        err = fclose(pfile);
+    }
+    else
+        printf("Impossibile aprire il file!");
+
+    return 0;
+}
