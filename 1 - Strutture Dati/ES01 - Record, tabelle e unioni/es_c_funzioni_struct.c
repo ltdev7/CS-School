cd4cadc5b98b6ab221e156c1938983cd45d60de3
lau7dev <103751178+lau7dev@users.noreply.github.com>
2024-10-23 19:19:06 +0000

    update

--- 1 - Strutture Dati/ES01 - Record, tabelle e unioni/es_c_funzioni_struct.c
+++ 1 - Strutture Dati/ES01 - Record, tabelle e unioni/es_c_funzioni_struct.c
@@ -11,13 +11,6 @@
 #define NUM_VOTI 5
 #define NUM_STUD 5
 
-void caricaTab(Studente t[]); //Carica la tabella chiedendo dati all'utente o generandoli casualmente.
-void stampaTab(Studente t[]); //Stampa la tabella allineando correttamente i dati.
-int ricercaCogn(Studente t[], char cogn[]); //Cerca uno studente per cognome, restituendo l'indice (o -1 se non trovato).
-int stampaMedia(Studente t[]); //Stampa la media voti per ogni studente e conta quante medie sono ≥ 6.
-void formattaCogn(Studente t[]); //Scrivi in maiuscolo la prima lettera del campo cognome e minuscole tutte le altre per tutti i record della tabella
-int studenti10(Studente t[]); //Conta gli studenti con almeno un voto pari a 10.
-
 // Struttura per la data
 typedef struct Data {
   int giorno;
@@ -32,24 +25,52 @@ typedef struct {
   int voti[NUM_VOTI];
 } Studente;
 
-int main(){
+void caricaTab(Studente t[]); //Carica la tabella chiedendo dati all'utente o generandoli casualmente.
+void StampaTab(Studente t[]); //Stampa la tabella allineando correttamente i dati.
+int ricercaCogn(Studente t[], char cogn[]); //Cerca uno studente per cognome, restituendo l'indice (o -1 se non trovato).
+int stampaMedia(Studente t[]); //Stampa la media voti per ogni studente e conta quante medie sono ≥ 6.
+void formattaCogn(Studente t[]); //Scrivi in maiuscolo la prima lettera del campo cognome e minuscole tutte le altre per tutti i record della tabella
+int studenti10(Studente t[]); //Conta gli studenti con almeno un voto pari a 10.
+
+int main()
+{
     Studente Tab[NUM_STUD];
     caricaTab(Tab);
-    stampaTab(Tab);
+    StampaTab(Tab);
 
     return 0;
 }
 
-void caricaTab(Studente t[]){
-    for(int i = 0; i<NUM_STUD; i++){
-        printf("Inserisci il cognome del %d studente", i);
+void caricaTab(Studente t[])
+{
+    for(int i = 0; i < NUM_STUD; i++)
+    {
+        printf("Inserisci il cognome dello studente numero %d:\t", i +  1);
         scanf("%s", t[i].cognome);
+        printf("Inserisci il giorno di nascita dello studente numero %d:\t", i + 1);
+        scanf("%d", &t[i].data_nascita.giorno);
+        printf("Inserisci il mese di nascita dello studente numero %d:\t", i + 1);
+        scanf("%d", &t[i].data_nascita.mese);
+        printf("Inserisci l'anno di nascita dello studente numero %d:\t", i + 1);
+        scanf("%d", &t[i].data_nascita.anno);
+        for(int j = 0; j < NUM_VOTI; j++)
+        {
+          printf("Inserisci il %d voto:\t", j + 1);
+          scanf("%d", &t[i].voti[j]);
+        }
     }
 }
 
-void stampatab(Studente t[]){
-  for(int i = 0; i < NUM_STUD; i++){
+void StampaTab(Studente t[])
+{
+  for(int i = 0; i < NUM_STUD; i++)
+  {
     printf("%s\t", t[i].cognome);
-    printf("%d/%d/%d", t[i].data_nascita.giorno, t[i].data_nascita.mese , t[i].data_nascita.anno);
+    printf("%d/%d/%d\t", t[i].data_nascita.giorno, t[i].data_nascita.mese , t[i].data_nascita.anno);
+    for(int j = 0; j < NUM_VOTI; j++)
+    {
+      printf("%d ", t[i].voti[j]);
+    }
+    printf("\n");
   }
 }
