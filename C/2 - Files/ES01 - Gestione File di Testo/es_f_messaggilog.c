3eb61a478d021df56f1502ea816764d11d8b0b06
lau7dev <103751178+lau7dev@users.noreply.github.com>
2024-10-28 16:43:50 +0000

    es f done

--- C/2 - Files/ES01 - Gestione File di Testo/es_f_messaggilog.c
+++ C/2 - Files/ES01 - Gestione File di Testo/es_f_messaggilog.c
@@ -4,39 +4,32 @@
  * L'obiettivo di questo esercizio è creare un programma in linguaggio C che permetta di scrivere messaggi di log su un file di log. 
  * Questi messaggi di log possono contenere informazioni importanti sul funzionamento di un'applicazione o di un sistema
  * e sono spesso utilizzati per scopi di debugging, tracciatura, monitoraggio o registrazione di eventi rilevanti.
+ * (L'ESERCIZIO E' UN COPIA INCOLLA DI QUELLO DEL PROF MA CON MODIFICHE APPORTATE)
  * 
  * @author Lorenzo Tarragoni
  * @date 28/10/24
  */
 
-typedef struct
-{
-    int tm_year;
-    int tm_mon;
-    int tm_mday;
-    int tm_hour;
-    int tm_min;
-    int tm_sec;
-} tm;
-
-typedef struct
-{
-    tm ore;
-} time_t;
-
-
 #include <stdio.h>
+#include <time.h>
+
+void writeLog(FILE *logFile, const char *message);
 
 int main()
 {
-    char *NomeFile = "TestFiles/log.txt";
-    FILE *logFile  = fopen(NomeFile, "a");
+    char *nfile = "TestFiles/log.txt";
+   
+    FILE *logFile = fopen(nfile, "a");
+    if(logFile == NULL){perror("Impossibile aprire il file"); return 1;}
+
+    writeLog(logFile, "Messaggio di log 1");
+    writeLog(logFile, "Messaggio di log 2");
 }
 
-void writeLog(FILE *logFile, const char *message)
+void writeLog(FILE *logFile, const char *message) 
 {
     time_t currentTime;
-    tm *localTime;
+    struct tm *localTime;
     time(&currentTime);
     localTime = localtime(&currentTime);
 
@@ -44,3 +37,4 @@ void writeLog(FILE *logFile, const char *message)
             localTime->tm_year + 1900, localTime->tm_mon + 1, localTime->tm_mday,
             localTime->tm_hour, localTime->tm_min, localTime->tm_sec, message);
 }
+
