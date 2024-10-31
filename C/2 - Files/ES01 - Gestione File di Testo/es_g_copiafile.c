388e65d159bbc235ef4227d524e08b96afa7cb28
lau7dev <103751178+lau7dev@users.noreply.github.com>
2024-10-31 10:31:09 +0000

    esercizio g

--- C/2 - Files/ES01 - Gestione File di Testo/es_g_copiafile.c
+++ C/2 - Files/ES01 - Gestione File di Testo/es_g_copiafile.c
@@ -15,9 +15,17 @@ int main()
 {
     char *nomefile1 = "TestFiles/file_src";
     char *nomefile2 = "TestFiles/file_dst";
+    int c;
 
     FILE *pfile1 = fopen(nomefile1, "r");
-    if(pfile1 == NULL) {perror("Errore nell'apertura del file"; return 1;)}
+    if(pfile1 == NULL) {perror("Errore nell'apertura del file"); return 1;}
     FILE *pfile2 = fopen(nomefile2, "w");
-    if(pfile1 == NULL) {perror("Errore nell'apertura del file"; return 1;)}
+    if(pfile1 == NULL) {perror("Errore nell'apertura del file"); return 1;}
+
+    while((c = getc(pfile1)) != EOF) {putc(c,pfile2);}
+
+    fclose(pfile1);
+    fclose(pfile2);
+
+    return 0;
 }
