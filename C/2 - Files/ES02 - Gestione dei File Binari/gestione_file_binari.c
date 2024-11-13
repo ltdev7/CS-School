7e37b6e6686f6c696d258df63cc951fcc4181829
lau7dev <103751178+lau7dev@users.noreply.github.com>
2024-11-13 13:00:28 +0000

    ricorstruzione del file

--- C/2 - Files/ES02 - Gestione dei File Binari/gestione_file_binari.c
+++ C/2 - Files/ES02 - Gestione dei File Binari/gestione_file_binari.c
@@ -18,41 +18,35 @@ typedef struct
     char cognome[20];
 } persona;
 
-void scrivifile(char* nomefile, persona utente[], int n)
-void leggifile(char* nomefile)
+void scrivifile(char* nomefile);
+void leggifile(FILE* pfile);
+
+
 
 int main()
 {
-    char* nomefile = {"TestFiles/testo1.dat"};
-    char c;
-    persona utenti[N];
+    char* nomefile = {"TestFiles/testo1.dat"}; // char nomefile[] = {ciao}
 
-    for(int i = 0; i < N; i++)
-    {
-        printf("Inserisci il nome della persona numero %d", i);
-        scanf("%s", utenti[i].nome);
-        printf("Inserisci il cognome della persona numero %d", i);
-        scanf("%s", utenti[i].cognome);
-    }   
-
-    scrivifile(nomefile, utenti, N);   
+    scrivifile(nomefile);
 
     return 0;
 }
 
-void scrivifile(char nomefile[], persona utente[], int n)
+
+
+void scrivifile(char* nomefile)
 {
+    persona buffer[N];
+
     FILE* pfile = fopen(nomefile, "wb");
-    if (pfile == NULL) {perror("Errore nell'apertura del file"); return;}
 
-    fwrite(utente, sizeof(persona), n, pfile);
+    for(int i = 0; i < N; i++)
+    {
+        printf("Inserisci Nome: ");
+        scanf("%s", buffer[i].nome);
+        printf("Inserisci cognome: ");
+        scanf("%s", buffer[i].cognome);
 
-    fclose(pfile);
-}
-
-void leggifile(char* nomefile)
-{
-    int buffer = 1;
-
-    fread()
+        fwrite(&buffer, sizeof(persona), 1, pfile);
+    }
 }
