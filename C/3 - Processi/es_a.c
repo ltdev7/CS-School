f7ea51d0e34ad66c27687ea8c47254c7b315ddb1
lau7dev <103751178+lau7dev@users.noreply.github.com>
2024-12-20 09:46:49 +0000

    es 1

--- C/3 - Processi/es_a.c
+++ C/3 - Processi/es_a.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
 #include <math.h>
+#include <unistd.h>
 
 int main(){
     int cateto1 = 3, cateto2 = 4;
     pid_t pid;
 
     pid = fork();
-
-    if(pid == 1){
+    if(pid < 0){
         perror("Errore nell'utilizzo del fork");
         return 1;
-    }
-
-    if(pid > 0){
+    } else if(pid > 0) {
         double area = cateto1 * 0.5 * cateto2;
-        printf("Padre: L'area del triangolo è: %d", area);
-    }
-
-    if (pid < 0){
+        printf("Padre: L'area del triangolo è: %f\n", area);
+    } else { //if (pid == 0)
         double ipotenusa = sqrt(cateto1 * cateto1 + cateto2 * cateto2);
         double perimetro = cateto1 + cateto2 + ipotenusa;
-        printf("Figlio: Il perimetro del rettangolo è: %d", perimetro);
+        printf("Figlio: Il perimetro del rettangolo è: %f\n", perimetro);
     }
 }
