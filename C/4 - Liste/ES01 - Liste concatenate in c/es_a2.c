379fa36a4311f22323613fa268c03897aeb89d97
lau7dev <103751178+lau7dev@users.noreply.github.com>
2025-03-12 11:53:08 +0000

    up

---
+++ C/4 - Liste/ES01 - Liste concatenate in c/es_a2.c
@@ -0,0 +1,12 @@
+/**
+ * @file es_a2.c
+ * 
+ * Scrivere una funzione che conti il numero di nodi in una lista concatenata senza utilizzare un contatore interno alla classe.
+ * 
+ * @author Lorenzo Tarragoni
+ * @date 11/03/25
+ */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+ 
